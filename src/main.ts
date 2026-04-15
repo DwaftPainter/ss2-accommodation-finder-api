5c99f19ee663f44cc9136920cfbcd49933276553
Mac <mac@MacBook-Pro-cua-Mac.local>
2026-04-15 09:53:44 +0700

    fix: binding to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -46,9 +46,9 @@ async function bootstrap() {
     }),
   );
 
-  const port = 3000;
+  const port = process.env.PORT || 10000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Server running at http://localhost:${port}`);
 }
