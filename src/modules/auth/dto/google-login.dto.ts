278e03aa1d59be20144027118c348d3c74b0afdd
thanhcong <2301140014@ms.hanu.edu.vn>
2026-06-12 13:13:56 +0700

    chore: sync progress

--- src/modules/auth/dto/google-login.dto.ts
+++ src/modules/auth/dto/google-login.dto.ts
@@ -1,6 +1,39 @@
-import { IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class GoogleLoginDto {
+  @IsOptional()
   @IsString()
-  token!: string;
+  sub?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  given_name?: string;
+
+  @IsOptional()
+  @IsString()
+  family_name?: string;
+
+  @IsOptional()
+  @IsString()
+  nickname?: string;
+
+  @IsOptional()
+  @IsString()
+  picture?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  email_verified?: boolean;
+
+  @IsOptional()
+  @IsString()
+  authProvider?: string;
 }
